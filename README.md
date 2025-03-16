87b393231f843a81c7561041e54437b656f3dd10
Luisina Arraya <luisinalalu@gmail.com>
2025-03-15 21:55:05 -0300

    Asigna, agrega y verifica los nombres del sorteo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ChallengeAmigoSecreto
 Permite ingresar nombres y realizar un sorteo
 Agregamos los archivos bases del proyecto  HTML y CSS proporcionados por Alura 
+## Instrucciones de Uso
+1. Abre el archivo index.html
+2.Ingresa los nombres de los participantes.
+3. Haz clic en "Agregar" para agregar los nombres.
+4. Una vez ingresados los nombres haz clic en "Sortear".
+5.El resultado se motrará el nombre de la persona.
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.//
+let NomSorteados = [];
+//Asigna el texto a un elemnto html
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+//Funcion para agregar los nombres para el sorteo//
+function agregarAmigo(){
+    let nomDeAmigo = document.getElementById("amigo");
+    let amigo = nomDeAmigo.value;
+    //Valida que amigo no este vacio o que sea un numero \d metacaracter de 0 a 9 y test devuelve true de tener un num//
+    if (!amigo || /\d/.test(amigo))
+    {
+        alert(`Por favor, Escribe un nombre`);
+        return;
+    }
+    // Limpiar el resultado del sorteo si ya se ha realizado
+    let resultado = document.getElementById('resultado');
+    if (resultado.innerHTML !== '') {
+        resultado.innerHTML = '';
+        limpiarLista();
+    }       
+    NomSorteados.push(amigo);
+    console.log(NomSorteados);
+    limpiarCampo();
+    mostrarListaAmigos();  
+}
+
+
+function limpiarCampo() {
+    let amigo = document.getElementById('amigo');
+    amigo.value = '';
+    amigo.focus();
+}
+
+function limpiarLista() { 
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    NomSorteados = [];
+}
+
+//Sorteo 
+asignarTextoElemento('h1', 'Amigo Secreto');
+asignarTextoElemento('h2', 'Digite el nombre de sus amigos');
+agregarAmigo();
